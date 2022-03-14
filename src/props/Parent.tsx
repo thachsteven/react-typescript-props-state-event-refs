a907078c03e1ca8357858dffc1315d03c96a4a0f
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-03-14 23:34:50 +0700

    Props

---
+++ src/props/Parent.tsx
@@ -0,0 +1,9 @@
+import {Child, ChildAsFC} from "./Child"
+
+const Parent  = () => {
+    return <ChildAsFC color="red" onClick={()=> console.log("Clicked") }>
+
+    </ChildAsFC>
+}
+
+export default Parent
