d0cf9aadf635e23812e3c559e5b1a1bc9a6c3bf6
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-03-15 12:50:08 +0700

    State with example find user

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-import  ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 
 const App = () => {
-    return <div>
-        <GuestList />
+  return (
+    <div>
+      <UserSearch />
+      {/* <GuestList /> */}
     </div>
+  );
 };
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
