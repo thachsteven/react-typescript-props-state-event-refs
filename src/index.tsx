9dd12b6fbb4a52a7ca35770b108a947d58ddc129
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-03-15 13:48:26 +0700

    Event handler

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import ReactDOM from "react-dom";
-import GuestList from "./state/GuestList";
-import UserSearch from "./state/UserSearch";
+import EventComponent from "./event/EventComponent";
 
 const App = () => {
   return (
     <div>
-      <UserSearch />
-      {/* <GuestList /> */}
+      <EventComponent />
     </div>
   );
 };
