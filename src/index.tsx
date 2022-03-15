2e1565d7b14702f28be07f2aaeda3fcc1a1d5be8
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-03-15 14:44:45 +0700

    Refs

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from "react-dom";
 import EventComponent from "./event/EventComponent";
+import UserSearch from "./refs/UserSearch";
 
 const App = () => {
   return (
     <div>
-      <EventComponent />
+        <UserSearch />
     </div>
   );
 };
